7f0f5479e9e18eeb782057426ea556c991ddd515
Midas <midas0x@protonmail.com>
2021-08-15 14:46:06 +0200

    Add test for interaction with mainnet WETH

---
+++ src/WethTest.t.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.6;
+
+import "ds-test/test.sol";
+
+interface WETH {
+    function balanceOf(address) external returns (uint);
+    function deposit() external payable;
+}
+
+contract WethTest is DSTest {
+    WETH weth;
+
+    function setUp() public {
+        weth = WETH(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+    }
+
+    function testWrap() public {
+        assertEq(weth.balanceOf(address(this)), 0);
+        weth.deposit{value: 1 ether}();
+        assertEq(weth.balanceOf(address(this)), 1 ether);
+    }
+}
