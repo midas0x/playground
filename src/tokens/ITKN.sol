fc510ced1cb6ea0e967c66e463481146c0facd0c
Midas <midas0x@protonmail.com>
2021-10-02 19:26:08 +0200

    Extend ERC20 token and interface

--- src/tokens/ITKN.sol
+++ src/tokens/ITKN.sol
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.6;
 
-import "../../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import { IERC20 } from "../../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import { IERC20Metadata } from "../../lib/openzeppelin-contracts/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
-interface ITKN is IERC20 {
+interface ITKN is IERC20, IERC20Metadata {
     function mint(address account, uint256 amount) external;
 
     function faucet(uint256 amount) external;
--- src/tokens/TKN.sol
+++ src/tokens/TKN.sol
@@ -1,10 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.6;
 
-import "../../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
-import "../../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import { Ownable } from "../../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+import { ERC20 } from "../../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
-contract TKN is ERC20, Ownable {
+import { ITKN } from "./ITKN.sol";
+
+contract TKN is ITKN, ERC20, Ownable {
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {} // solhint-disable-line no-empty-blocks
 
     function mint(address account, uint256 amount) external override onlyOwner {
